a107f240ac1a4ab199bd35f1252cddb41a20ad39
Loic Sharma <sharma.loic@gmail.com>
2015-09-12 12:35:31 -0400

    Cleaned up Inspirations View Controller.

--- Pik Pak/InspirationsViewController.swift
+++ Pik Pak/InspirationsViewController.swift
@@ -6,47 +6,45 @@
 import UIKit
 
 class InspirationsViewController: UICollectionViewController {
-   
-    
-  let inspirations = Inspiration.allInspirations()
-  
-  override func preferredStatusBarStyle() -> UIStatusBarStyle {
-    return UIStatusBarStyle.LightContent
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    if let patternImage = UIImage(named: "Pattern") {
-      view.backgroundColor = UIColor(patternImage: patternImage)
-    }
-    collectionView!.backgroundColor = UIColor.clearColor()
-    
-   
-  }
 
+    let inspirations = Inspiration.allInspirations()
+  
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        if let patternImage = UIImage(named: "Pattern") {
+            view.backgroundColor = UIColor(patternImage: patternImage)
+        }
+    
+        collectionView!.backgroundColor = UIColor.clearColor()
+    }
 }
 
 extension InspirationsViewController {
-  
-  override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-    return 1
-  }
-  
-  override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    let count = inspirations.count
-    return count
-  }
-  
-  override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("InspirationCell", forIndexPath: indexPath) as! InspirationCell
-    cell.inspiration = inspirations[indexPath.item]
     
-    return cell
-  }
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+  
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return inspirations.count
+    }
+  
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("InspirationCell", forIndexPath: indexPath) as! InspirationCell
+        cell.inspiration = inspirations[indexPath.item]
+
+        return cell
+    }
+
     override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         let layout = collectionViewLayout as! UltravisualLayout
         let offset = layout.dragOffset * CGFloat(indexPath.item)
+        
         if collectionView.contentOffset.y != offset {
             collectionView.setContentOffset(CGPoint(x: 0, y: offset), animated: true)
         }
